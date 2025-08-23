ae817893d987b7779708f87dd5444a1974ac7da7
tonhissimo <torresmagalhaes.contato@outlook.com>
2025-08-23 19:24:02 -0300

    Adicionar arquivos de projeto.

---
+++ RentManagementAPI/Controllers/DriverController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentManagementAPI.Controllers
+{
+    public class DriverController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
